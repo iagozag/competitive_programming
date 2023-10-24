038066def1e95f35b5c66b23cd37615304926f59
iagozag <iagozag@gmail.com>
2023-10-24 20:38:20 -0300

    idk

--- lista_maratona_ufmg/dsu_kruskal/experience.cpp
+++ lista_maratona_ufmg/dsu_kruskal/experience.cpp
@@ -16,7 +16,7 @@ typedef vector<ll> vl;
 const int INF = 0x3f3f3f3f;
 const ll LINF = 0x3f3f3f3f3f3f3f3fll;
 
-vi id, sz, xp;
+vi id, sz, xp, xp_prev;
 
 int find(int x){
     return id[x] = (id[x] == x ? x : find(id[x]));
@@ -26,19 +26,22 @@ void unio(int a, int b){
     a = find(a), b = find(b);
     if(a == b) return;
     if(sz[a] > sz[b]) swap(a,b);
-    id[a] = b, sz[b] += a;
+    id[a] = b, sz[b] += a, xp_prev[b] = xp[b];
 }
 
 void solve(){
     int n, m; cin >> n >> m;
     id = vi(n); iota(all(id),0);
-    sz = vi(n,1); xp = vi(n);
+    sz = vi(n,1); xp = vi(n); xp_prev = vi(n);
     for(int i = 0; i < m; i++){
 	    string st; int a, b; cin >> st;
-	    if(st == "join") cin >> a >> b, a--, b--, unio(a,b);
-        else if(st == "get"){ 
-            cin >> a; a--; cout << xp[a] << endl;
-        } else{
+        if(st == "join") cin >> a >> b, a--, b--, unio(a, b);
+        else if(st == "get"){
+            cin >> a; a--;
+            if(id[a] != a) xp[a] += xp[id[a]] - xp_prev[id[a]];
+            cout << xp[a] << endl;
+        } 
+        else {
             cin >> a >> b; a--; a = find(a);
             xp[a] += b;
         }
