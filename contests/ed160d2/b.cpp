8cc01515d282e0e3cb9d5ec55f21d067d7c23874
iagozag <iagozag@gmail.com>
2023-12-20 00:58:04 -0300

    contest 916d3

---
+++ contests/ed160d2/b.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define all(a) (a).begin(), (a).end()
+#define endl '\n'
+#define f first
+#define s second
+#define pb push_back
+
+typedef long long ll;
+typedef pair<int,int> ii;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+void DBG() {
+    cerr << "]" << endl;
+}
+
+void DBGC() {
+    cerr << "]" << endl;
+}
+
+template<class H, class... T> void DBG(H h, T... t) {
+    cerr << to_string(h);
+    if(sizeof...(t)) cerr << ", ";
+    DBG(t...);
+}
+
+template<class H, class... T> void DBGC(H h, T... t) {
+    for(auto& x: h) cerr << x << " ";
+    if(sizeof...(t)) cerr << "], [ ";
+    DBGC(t...);
+}
+
+#ifndef _DEBUG
+#define dbg(...) cerr << "[" << #__VA_ARGS__ << "]: [", DBG(__VA_ARGS__)
+#define dbgc(...) cerr << "["<< #__VA_ARGS__ << "]: [ "; DBGC(__VA_ARGS__) 
+#else
+#define dbg(...) 0
+#define dbgc(...) 0
+#endif
+
+void solve(){
+    string st; cin >> st; int n = st.size();
+
+    vi cnt(2);
+    for(int i = 0; i < n; i++){
+        if(st[i] == '0') cnt[0]++;
+        else cnt[1]++;
+    }
+
+    int len = 0;
+    for(int i = 0; i < n; i++){
+        int cp = (st[i]-'0') ^ 1;
+        if(cnt[cp] == 0) break;
+        len++, cnt[cp]--;
+    }
+
+    cout << n-len << endl;
+}
+
+int main(){ _
+    int t; cin >> t;
+    while(t--){
+        solve();
+    }
+
+    exit(0);
+}
+
---
+++ contests/ed160d2/c.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define all(a) (a).begin(), (a).end()
+#define endl '\n'
+#define f first
+#define s second
+#define pb push_back
+
+typedef long long ll;
+typedef pair<int,int> ii;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+void DBG() {
+    cerr << "]" << endl;
+}
+
+void DBGC() {
+    cerr << "]" << endl;
+}
+
+template<class H, class... T> void DBG(H h, T... t) {
+    cerr << to_string(h);
+    if(sizeof...(t)) cerr << ", ";
+    DBG(t...);
+}
+
+template<class H, class... T> void DBGC(H h, T... t) {
+    for(auto& x: h) cerr << x << " ";
+    if(sizeof...(t)) cerr << "], [ ";
+    DBGC(t...);
+}
+
+#ifndef _DEBUG
+#define dbg(...) cerr << "[" << #__VA_ARGS__ << "]: [", DBG(__VA_ARGS__)
+#define dbgc(...) cerr << "["<< #__VA_ARGS__ << "]: [ "; DBGC(__VA_ARGS__) 
+#else
+#define dbg(...) 0
+#define dbgc(...) 0
+#endif
+
+bool isSubsetSum(vl& set, int n, int sum)
+{
+    // The value of subset[i][j] will be true if
+    // there is a subset of set[0..j-1] with sum
+    // equal to i
+    bool subset[n + 1][sum + 1];
+ 
+    // If sum is 0, then answer is true
+    for (int i = 0; i <= n; i++)
+        subset[i][0] = true;
+ 
+    // If sum is not 0 and set is empty,
+    // then answer is false
+    for (int i = 1; i <= sum; i++)
+        subset[0][i] = false;
+ 
+    // Fill the subset table in bottom up manner
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= sum; j++) {
+            if (j < set[i - 1])
+                subset[i][j] = subset[i - 1][j];
+            if (j >= set[i - 1])
+                subset[i][j]
+                    = subset[i - 1][j]
+                      || subset[i - 1][j - set[i - 1]];
+        }
+    }
+ 
+    return subset[n][sum];
+}
+
+void solve(){
+    int m; cin >> m;
+    vl set, queries2;
+    for(int i = 0; i < m; i++){
+        ll a, b; cin >> a >> b;
+        if(a == 1) set.pb(pow(2, b));
+        else cout << (isSubsetSum(set, set.size(), b) ? "YES" : "NO") << endl;
+    }
+}
+
+int main(){ _
+    int t = 1;
+    while(t--){
+        solve();
+    }
+
+    exit(0);
+}
+
